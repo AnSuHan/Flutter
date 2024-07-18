91cd65df8b3009ef85806996caf13b1f60f23dba
AnSuHan <glorygem195@gmail.com>
2024-07-18 19:07:52 +0900

    stash

--- FirebaseAuth/lib/main.dart
+++ FirebaseAuth/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'firebase_options.dart';
 
@@ -25,6 +26,7 @@ class _AuthState extends State<Auth> {
   String authState = "";
   String registerEmailState = "";
   String emailLoginOutState = "";
+  String googleRegister = "";
 
   @override
   Widget build(BuildContext context) {
@@ -85,7 +87,28 @@ class _AuthState extends State<Auth> {
                       }, child: const Text("logout")
                   ),
                 ],
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(googleRegister,
+                  style: const TextStyle(fontSize: 24, color: Colors.black),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        signInWithGoogle();
+                      }, child: const Text("google sign in")
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        signOutWithGoogle();
+                      }, child: const Text("google sign out")
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -138,5 +161,50 @@ class _AuthState extends State<Auth> {
         emailLoginOutState = 'Failed to logout: $e';
       });
     }
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      // 구글 로그인 인증 객체 생성
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      // 구글 계정 선택 창 표시
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+
+      if (googleSignInAccount != null) {
+        // Firebase에 구글 계정으로 로그인
+        final GoogleSignInAuthentication googleAuth = await googleSignInAccount.authentication;
+        final OAuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+        // 로그인 성공 시 처리
+        final User? user = userCredential.user;
+        setState(() {
+          googleRegister = '로그인 사용자: ${user!.displayName}';
+        });
+      } else {
+        // 구글 계정 선택 취소
+        setState(() {
+          googleRegister = '구글 로그인 취소됨';
+        });
+      }
+    } catch (e) {
+      // 오류 처리
+      setState(() {
+        googleRegister = '구글 로그인 실패: $e';
+      });
+    }
+  }
+
+  Future<void> signOutWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut(); // 구글 로그아웃 처리
+
+    await FirebaseAuth.instance.signOut(); // Firebase Authentication 로그아웃 처리
+    setState(() {
+      googleRegister = '로그아웃 완료';
+    });
   }
 }
