caccbdf019f2219dd13fbc6b495025bd23600cad
gloryoutput <gloryoutput@gmail.com>
2025-03-12 10:41:29 +0900

    import ios

---
+++ justAudioPlayerIos/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'StreamPlayer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Just Audio',
+      // home: JustAudioExample(),
+      home: StreamPlayer(),
+    );
+  }
+}
+
+class PositionData {
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+
+  PositionData(this.position, this.bufferedPosition, this.duration);
+}
